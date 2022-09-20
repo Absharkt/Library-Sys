1f0729a21ca7ac88c2cddf6a90c85548e7ba2e58
Absharkt <absharktabshar@gmail.com>
2022-09-20 10:03:38 +0530

    'second'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { AppComponent } from './app.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { BooksComponent } from './books/books.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { StudentsComponent } from './students/students.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'',redirectTo:'student',pathMatch:'full'},
+  { path: 'student', loadChildren: () => import('./student/student.module').then(mod => mod.StudentModule) },
+  { path:'admin',component:AdminComponent,
+    children:[
+      { path:'books',component:BooksComponent },
+      { path:'students',component:StudentsComponent },
+      { path:'book-details/:id',component:BookDetailsComponent },
+
+    ]},
+  { path:'**',component:PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AdminComponent } from './admin/admin.component';
+import { StudentsComponent } from './students/students.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    PageNotFoundComponent,
+    AdminComponent,
+    StudentsComponent,
+    BookDetailsComponent
   ],
   imports: [
     BrowserModule,
